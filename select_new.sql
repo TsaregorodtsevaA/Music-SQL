8fd0f077f29e7472bdf20036619b034cd2f9dac2
Aleksandra Tsaregorodtseva <alizandar@rambler.ru>
2022-10-20 13:52:59 +0300

    fixed 4th and 5th requests

--- select_new.sql
+++ select_new.sql
@@ -10,20 +10,21 @@ select albom.name, avg(duration)  from albom
 join track on track.albomid = albom.albomid
 group by albom.name;
 
-select singer.name from singer
+select DISTINCT singer.name from singer
 join singeralbom on singer.singerid = singeralbom.singerid
 join albom on singeralbom.albomid = albom.albomid
-where year!=2020
-group by singer.name;
+where singer.name not in (select singer.name from singer
+join singeralbom on singer.singerid = singeralbom.singerid
+join albom on singeralbom.albomid = albom.albomid
+where year=2020);
 
-select c.name from singer s
+select DISTINCT c.name from singer s
 join singeralbom s2 on s2.singerid = s.singerid
 join albom a on a.albomid = s2.albomid
 join track t on t.albomid = a.albomid
 join trackcollection t2 on t2.trackid  = t.trackid
 join collection c on c.collectionid = t2.collectionid
-where s.name = 'Queen'
-group by c.name;
+where s.name = 'Queen';
 
 select a.name from albom a 
 join singeralbom s on s.albomid = a.albomid
